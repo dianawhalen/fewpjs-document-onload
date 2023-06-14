34801cb80e05a2ceb8acb844434193cb80a6214b
dianawhalen <whalendianaj@gmail.com>
2023-06-14 15:45:52 -0600

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function() {
+    let textElement = document.getElementById('text');
+    if(textElement){
+       textElement.textContent = "This is really cool!";
+    }
+});
